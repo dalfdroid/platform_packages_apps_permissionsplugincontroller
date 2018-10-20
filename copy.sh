ec06d9474a5ca47d8811a49ded027ae54f7cd606
nisarg <nisarg@cs.duke.edu>
2018-10-20 05:27:07 -0400

    Redesign Permissions Controller App UI.
    Add functionality to select target packages and target APIs for plugin.
    
    This commit is part of the issue : https://gitlab.cs.duke.edu/Heimdall/ContextualPermissions/issues/20

--- copy.sh
+++ copy.sh
@@ -6,3 +6,6 @@ mv res ~/Projects/Heimdall/tmp/
 cp -r ~/Projects/Heimdall/PermissionsPluginController/app/src/main/java/com src/
 cp -r ~/Projects/Heimdall/PermissionsPluginController/app/src/main/res .
 
+rm src/com/android/permissionsplugin/PermissionsPlugin.java
+
+cp PackageManagerBridge.java src/com/android/permissionsplugin/permissionsplugincontroller/PackageManagerBridge.java
