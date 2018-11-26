08b40f055cb378fe667fcdcdd10138a559febf0a
nisarg <nisarg@cs.duke.edu>
2018-11-26 00:15:33 -0500

    Allow users to select target API for each target app.
    Issue: https://gitlab.cs.duke.edu/Heimdall/ContextualPermissions/issues/29

--- src/com/android/permissionsplugin/permissionsplugincontroller/PackageManagerBridge.java
+++ src/com/android/permissionsplugin/permissionsplugincontroller/PackageManagerBridge.java
@@ -23,29 +23,9 @@ public class PackageManagerBridge {
         return pm.getInstalledPermissionsPlugins();
     }
 
-    public static boolean setActivationStatusForPermissionsPlugin(PackageManager pm, String pluginPackage, boolean isActive){
-        Log.i(TAG,"setActivationStatusForPermissionsPlugin pluginPackage:" + pluginPackage + " isActive:" + isActive);
-        return pm.setActivationStatusForPermissionsPlugin(pluginPackage,isActive);
-    }
-
-    public static boolean addTargetPackagesForPlugin(PackageManager pm, String pluginPackage, List<String> targetPackages, boolean reset){
-        Log.i(TAG,"addTargetPackagesForPlugin pluginPackage:" + pluginPackage);
-        return pm.addTargetPackagesForPlugin(pluginPackage, targetPackages, reset);
-    }
-
-    public static boolean removeTargetPackagesForPlugin(PackageManager pm, String pluginPackage, List<String> targetPackages){
-        Log.i(TAG,"removeTargetPackagesForPlugin pluginPackage:" + pluginPackage);
-        return pm.removeTargetPackagesForPlugin(pluginPackage, targetPackages);
-    }
-
-    public static boolean addTargetAPIsForPlugin(PackageManager pm, String pluginPackage, List<String> targetAPIs, boolean reset){
-        Log.i(TAG,"addTargetAPIsForPlugin pluginPackage:" + pluginPackage);
-        return pm.addTargetAPIsForPlugin(pluginPackage,targetAPIs, reset);
-    }
-
-    public static boolean removeTargetAPIsForPlugin(PackageManager pm, String pluginPackage, List<String> targetAPIs){
-        Log.i(TAG,"removeTargetAPIsForPlugin pluginPackage:" + pluginPackage);
-        return pm.removeTargetAPIsForPlugin(pluginPackage, targetAPIs);
+    public static boolean activatePlugin(PackageManager pm, String pluginPackage, String targetPackage, String targetAPI, boolean activate){
+        Log.i(TAG,"activatePlugin pluginPackage:" + pluginPackage + " targetPackage:" + targetPackage + " targetAPI:" + targetAPI + " activate: " + activate);
+        return pm.activatePlugin(pluginPackage,targetPackage,targetAPI,activate);
     }
 
     public static List<String> getInstalledUntrustedPackages(PackageManager pm){
